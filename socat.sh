84373a3d403aa5c3138fc119831f5f7a564cf970
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-10-07 16:06:36 +0700

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -9,18 +9,17 @@ sudo ufw allow 8888
 
 # Bước 2: Tạo folder và file check_node_status.sh
 mkdir -p /home/user/scripts
-touch /home/user/scripts/check_node_status.sh
 
 # Bước 3: Tạo script kiểm tra trạng thái node Shardeum
-cat <<SCRIPT > /home/user/scripts/check_node_status.sh
+cat <<'SCRIPT' > /home/user/scripts/check_node_status.sh
 #!/bin/bash
 
 # Kiểm tra trạng thái của node Shardeum
-NODE_STATUS=\$(docker exec shardeum-dashboard operator-cli status)
+NODE_STATUS=$(docker exec shardeum-dashboard operator-cli status)
 
 # Tạo HTTP header và response dưới dạng JSON
 echo -e "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n"
-echo -e "{ \"status\": \"Success\", \"node_status\": \"\$(echo "\$NODE_STATUS" | sed ':a;N;\$!ba;s/\n/\\n/g')\" }"
+echo -e "{ \"status\": \"Success\", \"node_status\": \"$(echo "$NODE_STATUS" | sed ':a;N;$!ba;s/\n/\\n/g')\" }"
 SCRIPT
 
 # Bước 4: Cấp quyền thực thi cho check_node_status.sh
