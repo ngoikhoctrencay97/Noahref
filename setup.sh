6287eeb5277f225da0cae68424d9624520dacd0a
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-11-28 02:14:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Cập nhật hệ thống và nâng cấp
+# Phần 1: Cài đặt Docker và thêm nhóm
 sudo apt update && sudo apt upgrade -y
-
-# Cài đặt Docker
 sudo apt install docker.io -y
-
-# Thêm người dùng vào nhóm Docker
 sudo usermod -aG docker $USER
 
-# Kích hoạt nhóm Docker cho người dùng hiện tại
-newgrp docker
+# Phần 2: Tiếp tục mà không cần đăng xuất
+newgrp docker <<EONG
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh
+chmod +x installer.sh
+./installer.sh
+EONG
