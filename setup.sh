3c292f2649c5cd69fa6eaee18de0c896aeb2ea86
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-11-28 02:10:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,3 @@ sudo usermod -aG docker $USER
 
 # Kích hoạt nhóm Docker cho người dùng hiện tại
 newgrp docker
-
-# Cài đặt Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Tải xuống và cài đặt Shardeum Validator Dashboard
-curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh
-chmod +x installer.sh
-./installer.sh
