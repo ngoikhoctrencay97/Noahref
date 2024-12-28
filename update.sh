a1424b5985843461998cd37aba7adbba52b75a90
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-12-28 16:54:44 +0700

    Update and rename update.exp to update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Kiểm tra trạng thái node Shardeum
+STATUS=$(docker exec shardeum-dashboard operator-cli status | grep -i 'state: active')
+
+# Nếu trạng thái KHÔNG phải là "active", thực thi lệnh cài đặt
+if [ -z "$STATUS" ]; then
+    echo "State is not 'active'. Running installation script..."
+    curl -O https://raw.githubusercontent.com/shardeum/shardeum-validator/refs/heads/itn4/install.sh
+    chmod +x installer.sh
+    yes "" | ./installer.sh
+else
+    echo "State is 'active'. No action required."
+fi
