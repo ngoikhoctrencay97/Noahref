cdddb606de2e41374101ef784a06440a80276aee
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-12-28 15:52:50 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ STATUS=$(docker exec shardeum-dashboard operator-cli status | grep -i 'state: ac
 # Nếu trạng thái KHÔNG phải là "active", thực thi lệnh cài đặt
 if [ -z "$STATUS" ]; then
     echo "State is not 'active'. Running installation script..."
-    curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh
+    curl -O https://raw.githubusercontent.com/shardeum/shardeum-validator/refs/heads/itn4/install.sh && chmod +x install.sh && ./install.sh 
     chmod +x installer.sh
     yes "" | ./installer.sh
 else
