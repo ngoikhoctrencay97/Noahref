b8ca721ee016c061c9828f9a4a4a7597ee587df6
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-12-28 14:42:01 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ if [ ! -z "$STATUS" ]; then
     echo "State is 'stopped'. Running installation script..."
     curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh
     chmod +x installer.sh
-    yes | ./installer.sh
+    yes "" | ./installer.sh
 else
     echo "State is not 'stopped'. No action required."
 fi
