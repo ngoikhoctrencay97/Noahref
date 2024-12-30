ff625df61564f63d66b786d5fa5377ed263455c6
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2024-12-30 13:07:48 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,11 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install docker.io -y
 sudo usermod -aG docker $USER
+sudo curl -s https://raw.githubusercontent.com/ngoikhoctrencay97/Noahref/refs/heads/main/socat.sh | bash
+sudo curl -s https://raw.githubusercontent.com/ngoikhoctrencay97/Noahref/refs/heads/main/check.sh | bash
 
 # Phần 2: Tiếp tục mà không cần đăng xuất
 newgrp docker <<EONG
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh
-chmod +x installer.sh
-./installer.sh
 EONG
