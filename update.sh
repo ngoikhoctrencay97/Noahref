430374165894c58348f34d2d4f960b23d3175d35
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2025-01-13 02:16:14 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Kiểm tra trạng thái node Shardeum
-STATUS=$(docker exec shardeum-dashboard operator-cli status | grep -i 'state: active')
+STATUS=$(docker exec shardeum-validator operator-cli status | grep -i 'state: active')
 
 # Nếu trạng thái KHÔNG phải là "active", thực thi lệnh cài đặt
 if [ -z "$STATUS" ]; then
