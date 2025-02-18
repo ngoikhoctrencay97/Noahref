7615e30a0efa180f29661b3d308700ef327086e5
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2025-02-19 03:53:02 +0700

    Update setup_nexus.sh

--- setup_nexus.sh
+++ setup_nexus.sh
@@ -2,17 +2,19 @@
 
 # Tạo một phiên tmux mới
 tmux new-session -s setup -d
-# Tạo thư mục và tải tệp prover-id
+
+# Tạo thư mục và tệp prover-id
 tmux send-keys -t setup "mkdir -p /home/ubuntu/.nexus/" C-m
-tmux send-keys -t setup "curl -o /home/ubuntu/.nexus/prover-id https://raw.githubusercontent.com/ngoikhoctrencay97/Noahref/refs/heads/main/prover-id" C-m
+tmux send-keys -t setup "echo -n 'dEfAuLT1' > /home/ubuntu/.nexus/prover-id" C-m
 
-# Thực thi các lệnh bên trong phiên tmux
-tmux send-keys -t setup "yes | sudo apt update && yes | sudo apt upgrade -y" C-m
-tmux send-keys -t setup "yes | sudo apt install screen curl libssl-dev pkg-config build-essential git-all protobuf-compiler -y" C-m
-tmux send-keys -t setup "sudo apt update" C-m
-tmux send-keys -t setup "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y" C-m
-tmux send-keys -t setup "source /home/ubuntu/.cargo/env" C-m
-tmux send-keys -t setup "curl https://cli.nexus.xyz/ | sh" C-m C-m C-m
+# Cập nhật và nâng cấp hệ thống
+tmux send-keys -t setup "sudo apt update && sudo apt upgrade -y" C-m
 
-# Hiển thị phiên tmux để bạn theo dõi
+# Cài đặt các gói cần thiết
+tmux send-keys -t setup "sudo apt install -y screen curl libssl-dev pkg-config build-essential git-all protobuf-compiler" C-m
+
+# Cài đặt Rust, thiết lập môi trường và cài đặt Nexus CLI
+tmux send-keys -t setup "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && . /home/ubuntu/.cargo/env && curl https://cli.nexus.xyz/ | sh" C-m C-m
+
+# Hiển thị phiên tmux để theo dõi
 tmux attach-session -t setup
